4bea4fb3f21ba71fd62f536601903a95f1d6f029
hy.lee <rurouni24@gmail.com>
2021-04-15 22:22:31 +0900

    🔥 update 01-04

--- tutorial/Chapter01/04_request_body.py
+++ tutorial/Chapter01/04_request_body.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 import uvicorn
 
@@ -9,15 +9,36 @@ from pydantic import BaseModel, HttpUrl
 app = FastAPI()
 
 
+# 추가: Item 클래스
+class Item(BaseModel):
+    name: str
+    price: float
+    amount: int = 0
+
+
 class User(BaseModel):
     name: str
     password: str
     avatar_url: Optional[HttpUrl] = None
+    inventory: List[Item] = []  # 추가: inventory
 
 
 @app.post("/users")
 def create_user(user: User):
     return user
+
+
+@app.get("/users/me")
+def get_user():
+    fake_user = User(
+        name="FastCampus",
+        password="1234",
+        inventory=[
+            Item(name="전설 무기", price=1_000_000),
+            Item(name="전설 방어구", price=900_000),
+        ]
+    )
+    return fake_user
 
 
 if __name__ == "__main__":
