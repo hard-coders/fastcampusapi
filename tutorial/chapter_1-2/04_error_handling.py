94a9a2bddcbeaee794304f22f7891d1985182b46
Spike <rurouni24@gmail.com>
2021-04-27 14:16:23 +0900

    ⭐️ add 04_error_handling

---
+++ tutorial/chapter_1-2/04_error_handling.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.responses import JSONResponse
+
+
+class SomeError(Exception):
+    def __init__(self, name: str, code: int):
+        self.name = name
+        self.code = code
+
+    def __str__(self):
+        return f"<{self.name}> is occured. code: <{self.code}>"
+
+
+app = FastAPI()
+
+
+@app.exception_handler(SomeError)
+async def some_error_handler(request: Request, exc: SomeError):
+    return JSONResponse(
+        content={"message": f"error is {exc.name}"}, status_code=exc.code
+    )
+
+
+users = {
+    1: {"name": "Fast"},
+    2: {"name": "Campus"},
+    3: {"name": "API"},
+}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    if user_id not in users.keys():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User <{user_id}> is not exists.",
+        )
+    return users[user_id]
+
+
+@app.get("/error")
+async def get_error():
+    raise SomeError("Hello", 500)
